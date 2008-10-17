3c2d80ebe145934f6f9e0a3d0bca56a0df545179
trasher <trasher@c50e49c4-371d-49e5-9d48-f53858f61472>
2008-10-17 12:30:44 +0000

    Add frFR

--- trunk/Modules/AtlasLootFu/Locales.lua
+++ trunk/Modules/AtlasLootFu/Locales.lua
@@ -1,9 +1,15 @@
 ATLASLOOTFU_LEFTCLICK = "|cff1eff00Left-Click|r Browse Loot Tables";
 ATLASLOOTFU_SHIFTCLICK = "|cffff0000Shift-Click|r View Options";
 ATLASLOOTFU_LEFTDRAG = "|cffccccccLeft-Click + Drag|r Move Minimap Button";
-    
+
 if (GetLocale() == "deDE") then
     ATLASLOOTFU_LEFTCLICK = "|cff1eff00Linksklick|r Loot-Tabellen durchsuchen";
     ATLASLOOTFU_SHIFTCLICK = "|cffff0000Shift-Klick|r Optionen anzeigen";
     ATLASLOOTFU_LEFTDRAG = "|cffccccccLinksklick + Ziehen|r Minimap-Button bewegen";
+end
+
+if (GetLocale() == "frFR") then
+    ATLASLOOTFU_LEFTCLICK = "|cff1eff00Clic-Gauche|r Parcourir les tables de butin";
+    ATLASLOOTFU_SHIFTCLICK = "|cffff0000Shift-Clic|r Affiche les Options";
+    ATLASLOOTFU_LEFTDRAG = "|cffccccccClic-Gauche + Maintenir|r Minimap-Button bewegen";
 end
