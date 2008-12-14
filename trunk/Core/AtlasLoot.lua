9313a9186691268a5f54c8d313e5193a1e28cdc6
daviesh <daviesh@c50e49c4-371d-49e5-9d48-f53858f61472>
2008-12-14 05:27:35 +0000

    v5.02.04 locked in

--- trunk/Core/AtlasLoot.lua
+++ trunk/Core/AtlasLoot.lua
@@ -36,10 +36,10 @@ local AL = LibStub("AceLocale-3.0"):GetLocale("AtlasLoot");
 --Establish version number and compatible version of Atlas
 local VERSION_MAJOR = "5";
 local VERSION_MINOR = "02";
-local VERSION_BOSSES = "03";
+local VERSION_BOSSES = "04";
 ATLASLOOT_VERSION = "|cffFF8400AtlasLoot Enhanced v"..VERSION_MAJOR.."."..VERSION_MINOR.."."..VERSION_BOSSES.."|r";
 ATLASLOOT_CURRENT_ATLAS = "1.13.0";
-ATLASLOOT_PREVIEW_ATLAS = "1.12.3";
+ATLASLOOT_PREVIEW_ATLAS = "1.13.1";
 ATLASLOOT_POSITION = AL["Position:"];
 
 --Standard indent to line text up with Atlas text
