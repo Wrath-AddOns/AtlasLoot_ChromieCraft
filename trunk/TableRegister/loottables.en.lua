a7fed269bb8c9f692b8057c37437a0ed6b1665c7
daviesh <daviesh@c50e49c4-371d-49e5-9d48-f53858f61472>
2009-04-20 07:42:50 +0000

    Updated Lake Wintergrasp items

--- trunk/TableRegister/loottables.en.lua
+++ trunk/TableRegister/loottables.en.lua
@@ -1336,6 +1336,7 @@ AtlasLoot_TableNames = {};
 	AtlasLoot_TableNames["LakeWintergrasp1"] = { BabbleZone["Wintergrasp"], "AtlasLootWotLK" };
 	AtlasLoot_TableNames["LakeWintergrasp2"] = { BabbleZone["Wintergrasp"], "AtlasLootWotLK" };
 	AtlasLoot_TableNames["LakeWintergrasp3"] = { BabbleZone["Wintergrasp"], "AtlasLootWotLK" };
+    AtlasLoot_TableNames["LakeWintergrasp4"] = { BabbleZone["Wintergrasp"], "AtlasLootWotLK" };
 	AtlasLoot_TableNames["VentureBay1"] = { BabbleZone["Grizzly Hills"]..": "..AL["Venture Bay"], "AtlasLootWotLK" };
   -- Misc Other PvP
 	AtlasLoot_TableNames["PvP60Accessories1"] = { AL["PvP Accessories (Level 60)"], "AtlasLootOriginalWoW" };
