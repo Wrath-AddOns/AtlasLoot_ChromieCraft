227b99ad9704d0c0795152d97457e884013f661b
s8095324 <s8095324@c50e49c4-371d-49e5-9d48-f53858f61472>
2009-05-22 04:51:22 +0000

    zhTW & zhCN update

--- Modules/AtlasLootFu/Locales.lua
+++ Modules/AtlasLootFu/Locales.lua
@@ -19,3 +19,16 @@ ATLASLOOTFU_LEFTCLICK = "|cff1eff00Левый-Клик|r обзор таблиц
 ATLASLOOTFU_SHIFTCLICK = "|cffff0000Shift-Клик|r настройки";
 ATLASLOOTFU_LEFTDRAG = "|cffccccccЛевый-Клик + тащить|r перемещение кнопки";
 end
+
+if (GetLocale() == "zhTW") then
+ATLASLOOTFU_LEFTCLICK = "|cff1eff00左鍵|r 瀏覽物品掉落表格";
+ATLASLOOTFU_SHIFTCLICK = "|cffff0000Shift-點擊|r 查看選項";
+ATLASLOOTFU_LEFTDRAG = "|cffcccccc右鍵 + 拖曳|r 移動小地圖按鈕";
+end
+
+if (GetLocale() == "zhCN") then
+ATLASLOOTFU_LEFTCLICK = "|cff1eff00左键|r 浏览物品掉落表格";
+ATLASLOOTFU_SHIFTCLICK = "|cffff0000Shift-点击|r 查看选项";
+ATLASLOOTFU_LEFTDRAG = "|cffcccccc右键 + 拖曳|r 移动小地图按钮";
+end
+
