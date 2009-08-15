8ef5d7e5512d6642228c40639714c1f18e1adfd7
ThyrelR <ThyrelR@c50e49c4-371d-49e5-9d48-f53858f61472>
2009-08-15 15:23:08 +0000

    Added >Alone in the Darkness< loot from Yogg-Saron 10/25Man

--- trunk/Modules/WrathoftheLichKing/wrathofthelichking.lua
+++ trunk/Modules/WrathoftheLichKing/wrathofthelichking.lua
@@ -2791,6 +2791,8 @@ local BabbleZone = LibStub("LibBabble-Zone-3.0"):GetLookupTable();
 		{ 19, 46096, "", "=q4=Signet of Soft Lament", "=ds=#s13#"};
 		{ 20, 46097, "", "=q4=Caress of Insanity", "=ds=#h1#, #w6#"};
 		{ 21, 46067, "", "=q4=Hammer of Crushing Whispers", "=ds=#h2#, #w6#"};
+		{ 23, 0, "INV_Box_01", "=q6="..AL["Bonus Loot"], "=q5="..AL["Alone in the Darkness"]};
+		{ 24, 46312, "", "=q4=Vanquished Clutches of Yogg-Saron", "=ds=#s14#", "", "100%"};
 		Prev = "UlduarVezax";
 	};
 
@@ -2816,6 +2818,8 @@ local BabbleZone = LibStub("LibBabble-Zone-3.0"):GetLookupTable();
 		{ 23, 45534, "", "=q4=Seal of the Betrayed King", "=ds=#s13#"};
 		{ 24, 45535, "", "=q4=Show of Faith", "=ds=#s14#"};
 		{ 25, 45533, "", "=q4=Dark Edge of Depravity", "=ds=#h2#, #w1#", "", "1%"};
+		{ 27, 0, "INV_Box_01", "=q6="..AL["Bonus Loot"], "=q5="..AL["Alone in the Darkness"]};
+		{ 28, 45693, "", "=q4=Mimiron's Head", "=ds=#e12#", "", "100%"};
 		Prev = "UlduarVezax25Man";
 	};
 
