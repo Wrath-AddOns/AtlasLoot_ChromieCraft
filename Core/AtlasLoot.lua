df1dbfd20ded1a5dec23cca2813034f79135e625
Daviesh <Daviesh@c50e49c4-371d-49e5-9d48-f53858f61472>
2009-12-08 10:33:19 +0000

    v5.09.00 for Patch 3.3

--- Core/AtlasLoot.lua
+++ Core/AtlasLoot.lua
@@ -39,8 +39,8 @@ local AL = LibStub("AceLocale-3.0"):GetLocale("AtlasLoot");
 
 --Establish version number and compatible version of Atlas
 local VERSION_MAJOR = "5";
-local VERSION_MINOR = "08";
-local VERSION_BOSSES = "10";
+local VERSION_MINOR = "09";
+local VERSION_BOSSES = "00";
 ATLASLOOT_VERSION = "|cffFF8400AtlasLoot Enhanced v"..VERSION_MAJOR.."."..VERSION_MINOR.."."..VERSION_BOSSES.."|r";
 ATLASLOOT_CURRENT_ATLAS = "1.15.1";
 ATLASLOOT_PREVIEW_ATLAS = "1.15.2";
