9e11a47995a468c8c71e5e3151f4532f5ad74a7f
arith <arith@c50e49c4-371d-49e5-9d48-f53858f61472>
2010-10-17 12:58:59 +0000

    update Atlas version info

--- trunk/Core/AtlasLoot.lua
+++ trunk/Core/AtlasLoot.lua
@@ -41,7 +41,7 @@ local VERSION_BOSSES = "06";
 ATLASLOOT_VERSION = "|cffFF8400AtlasLoot Enhanced v"..VERSION_MAJOR.."."..VERSION_MINOR.."."..VERSION_BOSSES.."|r";
 --Now allows for multiple compatible Atlas versions.  Always put the newest first
 ATLASLOOT_CURRENT_ATLAS = {"1.17.2", "1.17.1"};
-ATLASLOOT_PREVIEW_ATLAS = {"1.17.0", "1.16.1"};
+ATLASLOOT_PREVIEW_ATLAS = {"1.18.0", "1.17.3"};
 
 ATLASLOOT_POSITION = AL["Position:"];
 ATLASLOOT_DEBUGMESSAGES = false;
