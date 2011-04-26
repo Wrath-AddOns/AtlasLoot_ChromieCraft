4ac1fda59593fe841e6ac36d0cc3b6a8fcdfac28
hegarol <hegarol@c50e49c4-371d-49e5-9d48-f53858f61472>
2011-04-26 07:13:21 +0000

    Atlas 1.19.0

--- trunk/Core/AtlasLoot.lua
+++ trunk/Core/AtlasLoot.lua
@@ -22,7 +22,7 @@ ATLASLOOT_VERSION_NUM = VERSION_MAJOR.."."..VERSION_MINOR.."."..VERSION_BOSSES
 --Now allows for multiple compatible Atlas versions.  Always put the newest first
 ATLASLOOT_MIN_ATLAS = "1.18.0"
 ATLASLOOT_CURRENT_ATLAS = {"1.18.3"};
-ATLASLOOT_PREVIEW_ATLAS = {"1.19.0", "1.18.4"};
+ATLASLOOT_PREVIEW_ATLAS = {"1.19.1", "1.19.0"};
 
 --ATLASLOOT_POSITION = AL["Position:"];
 ATLASLOOT_DEBUGMESSAGES = false;
