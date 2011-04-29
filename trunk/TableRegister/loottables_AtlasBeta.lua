d0a9ce4f3f4df0879abe793d3e716ef0c28221f1
dynaletik <dynaletik@c50e49c4-371d-49e5-9d48-f53858f61472>
2011-04-29 16:22:18 +0000

    - Atlas update

--- trunk/TableRegister/loottables_AtlasBeta.lua
+++ trunk/TableRegister/loottables_AtlasBeta.lua
@@ -67,7 +67,7 @@ AtlasLoot_LootTableRegister_AtlasBeta = {
 				["Bosses"] = {
 					{ "WailingCavernsLoot#1", {2,3,4,5,6} },
 					{ "WailingCavernsLoot#2", {7,8,9,10}, hide = true },
-					{ "VWOWSets#1", 13, hide = true },
+					{ "VWOWSets#1", 15, hide = true },
 				},
 				["Info"] = { BabbleZone["Wailing Caverns"], "AtlasLootClassicWoW", mapname = "WailingCaverns" },
 			},
