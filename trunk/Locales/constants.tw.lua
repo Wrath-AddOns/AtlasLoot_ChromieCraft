5071e4106e947d2af25694d71fbdbe2d5f26f5dd
ananhaid <ananhaid@c50e49c4-371d-49e5-9d48-f53858f61472>
2011-09-13 01:20:24 +0000

    AtlasLoot: zhTW update.

--- trunk/Locales/constants.tw.lua
+++ trunk/Locales/constants.tw.lua
@@ -681,6 +681,7 @@ if AL then
 	AL["Achievement & Faction Reward"] = "成就/聲望獎勵";
 	AL["Card Game Companions"] = "卡牌遊戲獎勵夥伴";
 	AL["Companion Accessories"] = "夥伴飾品";
+	AL["Blizzard Store"] = "暴雪商城";
 	AL["Crafted Companions"] = "製作的夥伴";
 	AL["Dungeon/Raid"] = "副本/團隊";
 	AL["Faction"] = "陣營";
