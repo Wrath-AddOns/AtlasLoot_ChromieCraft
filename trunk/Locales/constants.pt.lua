e70dd83d39615359be1939fe8081d8ad1977b34d
lag123 <lag123@c50e49c4-371d-49e5-9d48-f53858f61472>
2011-12-04 17:25:45 +0000

    - Add ptBR file (/Locales/constants.pt.lua)

---
+++ trunk/Locales/constants.pt.lua
@@ -0,0 +1,20 @@
+﻿-- $Id$
+--[[
+constants.pt.lua
+This file defines an AceLocale table for all the various text strings needed
+by AtlasLoot.  In this implementation, if a translation is missing, it will fall
+back to the English translation.
+
+The AL["text"] = true; shortcut can ONLY be used for English (the root translation).
+]]
+
+-- Create the library instance
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+
+local AL = AceLocale:NewLocale("AtlasLoot", "ptBR", false);
+
+-- Register translations
+if AL then
+	--AL["text"] = true;
+
+end
