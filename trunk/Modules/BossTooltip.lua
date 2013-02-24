550c532b29d272d3ced1e116cc2ddd338f342cc2
Bahnak <Bahnak@c50e49c4-371d-49e5-9d48-f53858f61472>
2013-02-24 18:40:32 +0000

    Updated difficulty detection.

--- trunk/Modules/BossTooltip.lua
+++ trunk/Modules/BossTooltip.lua
@@ -26,26 +26,12 @@ function AtlasLoot_hook(tooltip)
 						end
 						for ALindexWishlistItem,ALvalueWishlistItem in pairs(ALvalueWishlist[1]) do
 							local ALbossHandle, ALitemDifficulty = strsplit("#", ALvalueWishlistItem[6])
-							local _, _, ALdifficultyIndex, _, ALmaxPlayers = GetInstanceInfo()
+							local _, _, ALdifficultyIndex = GetInstanceInfo()
 							local ALinstanceDifficulty = "Normal"
-							if (ALmaxPlayers == 5 and ALdifficultyIndex == 2) or (ALmaxPlayers == 10 and ALdifficultyIndex == 3) or (ALmaxPlayers == 25 and ALdifficultyIndex == 4) then
+							if ALdifficultyIndex == 2 or ALdifficultyIndex == 5 or ALdifficultyIndex == 6 then
 								ALinstanceDifficulty = "Heroic"
-							----------------------------------------------------------------------------
-							-- Fix to detect LFR till Blizzard adds that difficulty to GetInstanceInfo()
-							----------------------------------------------------------------------------
-							else
-								if ALmaxPlayers == 25 then
-									for i=1,25 do
-										local _, ALrealm = UnitName("raid"..i)
-										if ALrealm and ALrealm~="" then
-											ALinstanceDifficulty = "RaidFinder"
-											break
-										end
-									end
-								end
-							----------------------------------------------------------------------------
-							-- End fix
-							----------------------------------------------------------------------------
+							elseif ALdifficultyIndex == 7 then
+								ALinstanceDifficulty = "RaidFinder"
 							end
 							if ALindexBoss == ALbossHandle and ALinstanceDifficulty == ALitemDifficulty then
 								local ALitemName, _, ALitemQuality, _, _, _, _, _, ALequipSlot = GetItemInfo(ALvalueWishlistItem[2])
