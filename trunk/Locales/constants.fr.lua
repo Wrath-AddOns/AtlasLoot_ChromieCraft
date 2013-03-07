040e4f39965a4140964cf9fbea064f7d42afc902
TrAsHeR <TrAsHeR@c50e49c4-371d-49e5-9d48-f53858f61472>
2013-03-07 02:23:33 +0000

    frFR update

--- trunk/Locales/constants.fr.lua
+++ trunk/Locales/constants.fr.lua
@@ -620,7 +620,7 @@ if AL then
 	AL["Pandaria World Bosses"] = "World Boss de Pandarie";
 	--AL["Elite"] = true;
 	AL["Vegetables"] = "Légumes";
-	AL["Thunderforged"] = "Foudroyants"; -- à traduire
+	AL["Thunderforged"] = "Foudroyant";
 
 	-- Loot Table Names
 	AL["Level %s"] = "Niveau %s"; -- usage: string.format(AL["Level %s"], "85-89")
