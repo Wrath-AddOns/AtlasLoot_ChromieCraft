43ae4f614064a3f9bdf367ff527f282382bbf3a3
Lag123 <Lag123@c50e49c4-371d-49e5-9d48-f53858f61472>
2014-02-17 11:00:12 +0000

    - small fixes

--- trunk/Core/Panel.lua
+++ trunk/Core/Panel.lua
@@ -399,12 +399,12 @@ do
 		
 		local SearchFrame = ButtonListLines["SearchFrame"]
 		
-		SearchFrame.SearchBox = CreateFrame("EditBox", "AtlasLootPanelSearch_Box", SearchFrame, "InputBoxTemplate")
+		SearchFrame.SearchBox = CreateFrame("EditBox", "AtlasLootPanelSearch_Box", SearchFrame, "SearchBoxTemplate")
 		SearchFrame.SearchBox:SetPoint("TOPLEFT", SearchFrame, "TOPLEFT")
 		SearchFrame.SearchBox:SetWidth(250)
 		SearchFrame.SearchBox:SetHeight(35)
 		SearchFrame.SearchBox:SetAutoFocus(false)
-		SearchFrame.SearchBox:SetTextInsets(0, 8, 0, 0)
+		--SearchFrame.SearchBox:SetTextInsets(0, 8, 0, 0)
 		SearchFrame.SearchBox:SetMaxLetters(100)
 		SearchFrame.SearchBox:SetScript("OnEnterPressed",function(self) 
 								AtlasLoot:CompareFrame_Search(SearchFrame.SearchBox:GetText(), "save")
